e7cd62c3be53b3323affcd1f02c4d0a2707c81c2
Alejandro Pallares <alejandrogp@dagsasc.com>
2024-02-08 20:51:39 -0600

    Add form & search view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,12 +1,14 @@
 {
-    'name': 'Tech Tr Estate',     # The name that will appear in the App list
-    'version': '16.0.0',  # Version
-    'application': True,  # This line says the module is an App, and not a module
-    'depends': ['base'],  # dependencies
+    'name': 'Tech Tr Estate',   # The name that will appear in the App list
+    'version': '16.0.0',        # Version
+    'application': True,        # This line says the module is an App, and not a module
+    'depends': ['base'],        # dependencies
     'data': [
         'views/estate_views.xml',
         'views/estate_menus.xml',
-        'views/estate_list_view.xml',
+        'views/estate_list_view.xml',        
+        'views/estate_form_view.xml',
+        'views/estate_search_view.xml',        
         'security/ir.model.access.csv',
     ],
     'installable': True,
